a21613aa315c5173fcf5ff4c5e4147befceaee3e
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-06-19 17:28:25 +0300

    Add files via upload
    home work in variables from pirple coding academy

---
+++ varhomework.py
@@ -0,0 +1,13 @@
+# Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
+# Now, within that file, list all of the attributes of the song, one after another, by creating variables for each attribute, and giving each variable a value. Here's an example:
+# Give each variable its own line. Then, after you have listed the variables, print each one of them out.
+genre = "Bongo Music"
+Artist = "Alikiba"
+Duration = 3.55
+Year_of_release = 2019
+song = "Mshumaah"
+print(genre)
+print(Artist)
+print(Duration)
+print(Year_of_release)
+print(song)
