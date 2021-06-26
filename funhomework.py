adc170266a70fbea79ef4ce5506ba9aa1a45f998
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-06-26 13:38:24 +0300

    Add files via upload
    homework on functions

---
+++ funhomework.py
@@ -0,0 +1,22 @@
+# creating function of music and calling it
+def genre():
+    print("bongo flavor")
+    print("genge tone")
+    print("rhumba")
+
+
+
+
+def Artist():
+    print("otile brown")
+    print("Alikiba")
+    print("willy paul")
+
+def Year():
+    print(2019)
+    print(2020)
+    print(2021)
+
+genre()
+Year()
+Artist()
