a3fad6b6815ffcd3f0e4ccb8af2ab65efbcada27
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-07-01 16:27:36 +0300

    Add files via upload
    creating shapes using loops in pirple academy lecture

---
+++ patterns.py.py
@@ -0,0 +1,11 @@
+# creating a right angle triangle
+
+size = 10
+for pos in range(1, size+ 1):
+    print("*" * pos)
+
+
+# creating inverse right angle triangle
+
+for pos in range(size, 0, -1):
+    print("*" * pos)
