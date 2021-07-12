4030e6bcb20dad7349f11b6065573152df053732
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-07-12 15:31:59 +0300

    Add files via upload
    dictionary and sets

--- dict_set.py
+++ dict_set.py
@@ -1,31 +1,31 @@
 # sets
 # syntax
 # sets do not maintain the order of elements
-# sets = {"element1", "element2", "element3", "element1"}
+sets = {"element1", "element2", "element3", "element1"}
 # sets do not repeat any elements
-# print(sets)
-# if "element1" in sets:
-#     print("yes")
-# else:
-#     print("No")
+print(sets)
+if "element1" in sets:
+    print("yes")
+else:
+    print("No")
 
 # converting a list to a set
 
-# country_list = []
-# for person in range(10):
-#     country = input("enter your country: ")
-#     country_list.append(country)
-#
-# country_set = set(country_list)
-# print(country_list)
-# print(country_set)
-# print()
+country_list = []
+for person in range(10):
+    country = input("enter your country: ")
+    country_list.append(country)
+
+country_set = set(country_list)
+print(country_list)
+print(country_set)
+print()
 
 # DICTIONARIES
 # syntax
 # adding value from the list into into the dictionary
 nations_list = []
-nation_dictionary = {"l" : 8}
+nation_dictionary = {}
 for individual in range(5):
     nation = input("enter nation: ")
     nations_list.append(nation)
