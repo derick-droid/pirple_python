00990401617bc8684c977e15559bddd263c98841
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-08-18 06:19:21 +0300

    Add files via upload
    introduction to imports in python from pirple tutorial lecture

---
+++ randomz.py
@@ -0,0 +1,14 @@
+# printing random number
+
+import random
+randintInt = random.randint(34, 100) # displaying interger values
+print(randintInt)
+
+# displaying floating value
+
+randFloat = random.random()
+print(randFloat)
+
+# get uniform float result
+randUniform = random.uniform(1, 1000)
+print(randUniform)
