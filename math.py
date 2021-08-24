be81854b1b50bbcd1a193867d560ef2d7afdda96
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-08-24 14:30:17 +0300

    Add files via upload
    maths library in python

---
+++ math.py
@@ -0,0 +1,22 @@
+import math
+# square root
+value = 9
+srtval = math.sqrt(value)
+print(srtval)
+# exponential
+expoval = math.exp(value)
+print(expoval)
+# factorial of a value
+factvalue = math.factorial(value)
+print(factvalue)
+# sin of a value
+sinvalue = math.sin(value)
+print(sinvalue)
+
+# floor of a value
+floorvalue = math.floor(value)
+print(floorvalue)
+# ceiling of a value
+ceilvalue = math.ceil(value)
+
+print(ceilvalue)
